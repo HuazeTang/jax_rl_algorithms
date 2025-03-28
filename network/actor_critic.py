e7cfc85037d9900d7d5714de11671d2a3237ee1c
tanghuaze02 <tanghuaze02@meituan.com>
2025-03-28 11:37:01 +0800

    feat(network): seperate the actor and critic

--- network/actor_critic.py
+++ network/actor_critic.py
@@ -9,17 +9,17 @@ from typing import Sequence, Tuple, Union, Any, Dict
 
 from utils.mixed_precision import mixed_precision
 
-class ActorCritic(nn.Module):
+class Actor(nn.Module):
     action_dim: Sequence[int]
     activation: str = "tanh"
 
-    # @mixed_precision()
     @nn.compact
     def __call__(self, x):
         if self.activation == "relu":
             activation = nn.relu
         else:
             activation = nn.tanh
+        
         actor_mean = nn.Dense(
             256, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
         )(x)
@@ -34,6 +34,18 @@ class ActorCritic(nn.Module):
         actor_logtstd = self.param("log_std", nn.initializers.zeros, (self.action_dim,))
         pi = distrax.MultivariateNormalDiag(actor_mean, jnp.exp(actor_logtstd))
 
+        return pi 
+
+
+class Critic(nn.Module):
+    activation: str = "tanh"
+
+    @nn.compact
+    def __call__(self, x):
+        if self.activation == "relu":
+            activation = nn.relu
+        else:
+            activation = nn.tanh
         critic = nn.Dense(
             256, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
         )(x)
@@ -46,7 +58,24 @@ class ActorCritic(nn.Module):
             critic
         )
 
-        return pi, jnp.squeeze(critic, axis=-1)
+        return jnp.squeeze(critic, axis=-1)
+
+
+class ActorCritic(nn.Module):
+    action_dim: Sequence[int]
+    activation: str = "tanh"
+
+    def setup(self):
+        self.actor = Actor(action_dim=self.action_dim, activation=self.activation)
+        self.critic = Critic(activation=self.activation)
+
+    # @mixed_precision()
+    @nn.compact
+    def __call__(self, x):
+        pi = self.actor(x)
+        critic = self.critic(x)
+
+        return pi, critic
 
 def init_network(
         activation_type: str, rng: jax.Array, action_dim: int, obs_shape: Tuple
